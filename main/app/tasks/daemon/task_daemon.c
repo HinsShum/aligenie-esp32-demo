48c161db3f4ff7af85b231ff6123334da9a68906
hinsshum <hinsshum@qq.com>
2023-01-10 21:49:39 +0800

    build: new empty project created

---
+++ main/app/tasks/daemon/task_daemon.c
@@ -0,0 +1,37 @@
+/**
+ * @file main/app/tasks/daemon/task_daemon.c
+ *
+ * Copyright (C) 2023
+ *
+ * task_daemon.c is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author HinsShum hinsshum@qq.com
+ *
+ * @encoding utf-8
+ */
+
+/*---------- includes ----------*/
+#include "options.h"
+
+/*---------- macro ----------*/
+/*---------- type define ----------*/
+/*---------- variable prototype ----------*/
+/*---------- function prototype ----------*/
+/*---------- variable ----------*/
+/*---------- function ----------*/
+void app_main(void)
+{
+    for(;;) {
+    }
+}
